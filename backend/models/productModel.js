14fca9f6bec1f9d014b5a9f75e994e06e66a927e
vedikaJ2012 <vedikamjadhav2004@gmail.com>
2026-02-13 09:57:13 +0530

    Products logic implementation

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,6 +1,6 @@
 import mongoose  from "mongoose";
 
-const productSchema = new mongoose.schema({
+const productSchema = new mongoose.Schema({
     name: {type:String, required:true},
     description: {type:String, required:true},
     price: {type:Number, required:true},
